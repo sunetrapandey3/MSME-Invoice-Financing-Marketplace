9777481c216649282583c615aa549bdad9225767
Sunetra <pandeysunetra7@gmail.com>
2025-12-28 01:22:38 +0530

    created app.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import UploadInvoice from "./components/UploadInvoice";
+import Marketplace from "./components/Marketplace";
+
+export default function App() {
+  return (
+    <>
+      <h1>DE-FI AGENTS 💸</h1>
+      <UploadInvoice />
+      <Marketplace />
+    </>
+  );
+}
